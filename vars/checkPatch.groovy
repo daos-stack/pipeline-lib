be9b3b89cd92215fb5cf2978b28c1a00fa6dc799
John.E.Malmberg <john.e.malmberg@intel.com>
2018-11-05 17:20:00 -0600

    CORCI-502 checkPatch Groovy only for Production
    * Add groovydoc
    * Require env.DAOS_JENKINS_NOTIFY_STATUS for an actual update.
    * Pass the credential ID for the updating the github review.
    * Provide a default empty Map as a parameter.
    
    Signed-off-by: John.E.Malmberg <john.e.malmberg@intel.com>

--- vars/checkPatch.groovy
+++ vars/checkPatch.groovy
@@ -8,6 +8,14 @@
 
 import com.intel.checkoutScm
 
+private def run_review(usr, psw, ignored_files) {
+  script = 'GH_USER="' + usr + '"' + \
+    ' GH_PASS="' + psw + '"' + \
+    ' CHECKPATCH_IGNORED_FILES="' + ignored_files + '"' + \
+    ' jenkins/code_review/jenkins_github_checkwarn.sh'
+    return sh(script: script, returnStatus: true)
+}
+
 /**
  * Method to check the status of a patch and notify GitHub of the results
  * 
@@ -19,6 +27,7 @@ import com.intel.checkoutScm
  *
  */
 def call(Map config = [:]) {
+
     if (env.DAOS_JENKINS_NOTIFY_STATUS == null) {
       println "Jenkins not configured to notify github of builds."
       return
@@ -26,7 +35,7 @@ def call(Map config = [:]) {
 
     def c = new com.intel.checkoutScm()
     c.checkoutScmWithSubmodules()
-
+    println "checked out scm with submodules"
     if (config['jenkins_review']) {
         rev_num = config['jenkins_review']
         branch = 'FETCH_HEAD'
@@ -61,25 +70,18 @@ def call(Map config = [:]) {
                  context: "pre-build" + "/" + env.STAGE_NAME,
                  status: "PENDING"
 
+    println "Starting fake review"
     rc = 1
     if (config['review_creds']) {
       withCredentials([[$class: 'UsernamePasswordMultiBinding', 
-                      credentialsId: 'config['review_creds'],
+                      credentialsId: config['review_creds'],
                       usernameVariable: 'GITHUB_USR',
                       passwordVariable: 'GITHUB_PSW']]) {
-        script = 'GH_USER="' + GITHUB_USR + '"' + \
-                 ' GH_PASS="' + GITHUB_PSW + '"' + \
-                 ' jenkins/code_review/jenkins_github_checkwarn.sh'
-        rc = sh(script: script, returnStatus: true)
+        rc = run_review(GITHUB_USR, GITHUB_PSW, ignored_files)
       }
     } else {
       // Older method to be deprecated
-
-      script = 'GH_USER="' + config['user'] + '"' + \
-               ' GH_PASS="' + config['password'] + '"' + \
-               ' CHECKPATCH_IGNORED_FILES="' + ignored_files + '"' + \
-               ' jenkins/code_review/jenkins_github_checkwarn.sh'
-      rc = sh(script: script, returnStatus: true)
+      rc = run_review(GITHUB_USR, GITHUB_PSW, ignored_files)
     }
 
     // All of this really should be done in the post section of the main
