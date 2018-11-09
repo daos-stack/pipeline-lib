412cdac89f2dab6395a70023f070597242f7e022
John E. Malmberg <14278277+JohnMalmberg@users.noreply.github.com>
2018-11-08 23:18:58 -0600

    CORCI-502 checkPatch Groovy only for Production (#4)
    * CORCI-502 checkPatch Groovy only for Production
    
    * Add groovydoc
    * Require env.DAOS_JENKINS_NOTIFY_STATUS for an actual update.
    * Lookup the credentials for the GITHUB upddate instead of having
      them passed.
    * Provide a default empty Map as a parameter.
    
    Signed-off-by: John.E.Malmberg <john.e.malmberg@intel.com>

--- vars/checkPatch.groovy
+++ vars/checkPatch.groovy
@@ -1,12 +1,32 @@
 // vars/checkPatch.groovy
 
+/**
+ * checkPatch.groovy
+ *
+ * checkPatch variable
+ */
+
 import com.intel.checkoutScm
 
-def call(Map config) {
+/**
+ * Method to check the status of a patch and notify GitHub of the results
+ * 
+ * @param config Map of parameters passed.
+ *
+ * config['ignored_files'] is colon delimited string of files not to check.
+ * config['jenkins_review'] is the optional revision number to use.
+ * config['review_creds'] is the credentials needed for a code review.
+ *
+ */
+def call(Map config = [:]) {
+
+    if (env.DAOS_JENKINS_NOTIFY_STATUS == null) {
+      println "Jenkins not configured to notify github of builds."
+      return
+    }
 
     def c = new com.intel.checkoutScm()
     c.checkoutScmWithSubmodules()
-
     if (config['jenkins_review']) {
         rev_num = config['jenkins_review']
         branch = 'FETCH_HEAD'
@@ -25,7 +45,6 @@ def call(Map config) {
     checkout([
         $class: 'GitSCM',
         branches: [[name: branch]],
-        doGenerateSubmoduleConfigurations: false,
         extensions: [[$class: 'RelativeTargetDirectory',
                               relativeTargetDir: 'jenkins']],
         submoduleCfg: [],
@@ -41,11 +60,23 @@ def call(Map config) {
                  context: "pre-build" + "/" + env.STAGE_NAME,
                  status: "PENDING"
 
-    script = 'GH_USER="' + config['user'] + '"' + \
-             ' GH_PASS="' + config['password'] + '"' + \
-             ' CHECKPATCH_IGNORED_FILES="' + ignored_files + '"' + \
+    rc = 1
+    script = 'CHECKPATCH_IGNORED_FILES="' + ignored_files + '"' + \
              ' jenkins/code_review/jenkins_github_checkwarn.sh'
-    rc = sh(script: script, returnStatus: true)
+
+    if (config['review_creds']) {
+      withCredentials([[$class: 'UsernamePasswordMultiBinding', 
+                      credentialsId: config['review_creds'],
+                      usernameVariable: 'GH_USER',
+                      passwordVariable: 'GH_PASS']]) {
+        rc = sh(script: script, returnStatus: true)
+      }
+    } else {
+      // Alternate method using username/password
+      script = 'GH_USER="' + config['user'] + '"' + \
+        ' GH_PASS="' + config['password'] + '" ' + script
+      rc = sh(script: script, returnStatus: true)
+    }
 
     // All of this really should be done in the post section of the main
     // Jenkinsfile but it cannot due to
