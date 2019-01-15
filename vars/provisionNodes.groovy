bd88a21af4364b3d83b5300a17afb1203ad6641b
Brian J. Murrell <brian@interlinx.bc.ca>
2019-01-15 13:23:23 -0500

    golang-bin is needed by the DAOS tests (#18)
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- vars/provisionNodes.groovy
+++ vars/provisionNodes.groovy
@@ -115,12 +115,14 @@ def call(Map config = [:]) {
                               echo \\"jenkins ALL=(ALL) NOPASSWD: ALL\\" > /etc/sudoers.d/jenkins
                               yum copr -y enable jhli/ipmctl
                               yum copr -y enable jhli/safeclib
+                              yum install -y epel-release
                               yum install -y openmpi CUnit fuse python34-PyYAML python34-nose    \
                                              python34-pip valgrind python34-paramiko             \
                                              python2-avocado python2-avocado-plugins-output-html \
                                              python2-avocado-plugins-varianter-yaml-to-mux       \
                                              python-debuginfo python2-aexpect libcmocka          \
-                                             yum-plugin-copr python-pathlib python2-numpy git
+                                             yum-plugin-copr python-pathlib python2-numpy git    \
+                                             golang-bin
                               yum copr -y enable jhli/ipmctl
                               yum copr -y enable jhli/safeclib
                               yum install -y libipmctl'''
