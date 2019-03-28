10aceeae6d904c11e6706d7c3dea9b85022a35b8
Brian J. Murrell <brian.murrell@intel.com>
2019-03-28 08:58:38 -0400

    A few more fixes
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- vars/provisionNodes.groovy
+++ vars/provisionNodes.groovy
@@ -122,12 +122,12 @@ def call(Map config = [:]) {
                                              python-pathlib python2-numpy git                    \
                                              golang-bin'''
     def rc = 0
-    rc = sh(script: """set -x
+    rc = sh(script: """set -ex
                        rm -f ci_key*
                        ssh-keygen -N "" -f ci_key
                        pdcp -R ssh -l root -w ${nodeString} ci_key* /tmp/
                        pdsh -R ssh -l root -w ${nodeString} """" +
-                    provision_script} + "''' 2>&1 | dshbak -c
+                    provision_script + '''" 2>&1 | dshbak -c
                        exit ${PIPESTATUS[0]}''',
             label: "Post provision configuration",
             returnStatus: true)
