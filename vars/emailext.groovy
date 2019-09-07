0efb7fc5d8abd87403a53ef20effb8c7017c150b
Brian J. Murrell <brian.murrell@intel.com>
2019-09-07 13:17:01 -0400

    Add emailextDaos; emailext wrapper for staging
    As a wrapper around emailext which has an option to not operate on PRs.
    
    Additionally, make emailext quiet on staging systems
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

---
+++ vars/emailext.groovy
@@ -0,0 +1,20 @@
+// vars/emailext.groovy
+
+/**
+ * emailext.groovy
+ *
+ * Wrapper for emailext step to allow quiet operation for a staging
+ * system.
+ *
+ */
+
+def call(Map config = [:]) {
+    println "Entering emailext override."
+
+    if (env.DAOS_JENKINS_NOTIFY_STATUS == null) {
+        println "Jenkins not configured to notify users of failed builds."
+        return
+    }
+
+    steps.emailext(config)
+}
