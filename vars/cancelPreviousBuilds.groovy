db8d120a21cc2ade130b6509a2735f896e807be7
Brian J. Murrell <brian.murrell@intel.com>
2019-11-04 17:00:08 -0500

    CORCI-818 build: Require patch
    To allow skipping of hardware testing if specified by Jenkins.
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- vars/cancelPreviousBuilds.groovy
+++ vars/cancelPreviousBuilds.groovy
@@ -17,6 +17,15 @@ def call(Map config = [:]) {
    * @param config Map of parameters passed (none currently)
    * @return Nothing
    */
+
+  if (jobName() == "daos") {
+      if (sh(script: "git log | grep \"CORCI-818 build: Skip hardware testing option\""
+              label: "Check required commits",
+              returnStatus: true) != 0) {
+        error "Your PR needs to be merged with latest master before it will build in CI."
+      }
+  }
+
   def c = new com.intel.cancelPreviousBuildsInternal()
   return c.cancelPreviousBuildsInternal(config)
 }
