af4fdedcf49288a09022ec83c7cf04ff4113e912
Brian J. Murrell <brian.murrell@intel.com>
2020-04-27 16:07:13 -0400

    Add stage name to e-mail subject
    Also extend dockerBuildArgs() to do some steps optionally, defaulting to
    existing behaviour.
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- vars/dockerBuildArgs.groovy
+++ vars/dockerBuildArgs.groovy
@@ -18,29 +18,41 @@ int getuid() {
 }
 
 String call(Map config = [:]) {
+    Boolean cachebust = True
+    Boolean add_repos = True
+    if (config['cachebust']) {
+      cachebust = config['cachebust']
+    }
+    if (config['add_repos']) {
+      add_repos = config['add_repos']
+    }
     ret_str = " --build-arg NOBUILD=1 --build-arg UID=" + getuid() +
-              " --build-arg JENKINS_URL=$env.JENKINS_URL" +
-              " --build-arg CACHEBUST=${currentBuild.startTimeInMillis}"
+              " --build-arg JENKINS_URL=$env.JENKINS_URL"
+    if (cachebust) {
+      ret_str += " --build-arg CACHEBUST=${currentBuild.startTimeInMillis}"
+    }
 
-    if (env.REPOSITORY_URL) {
-      ret_str += ' --build-arg REPO_URL=' + env.REPOSITORY_URL
-    }
-    if (env.DAOS_STACK_EL_7_LOCAL_REPO) {
-      ret_str += ' --build-arg REPO_EL7=' + env.DAOS_STACK_EL_7_LOCAL_REPO
-    }
-    if (env.DAOS_STACK_EL_8_LOCAL_REPO) {
-      ret_str += ' --build-arg REPO_EL8=' + env.DAOS_STACK_EL_8_LOCAL_REPO
-    }
-    if (env.DAOS_STACK_LEAP_15_LOCAL_REPO) {
-      ret_str += ' --build-arg REPO_LOCAL_LEAP15=' +
-                 env.DAOS_STACK_LEAP_15_LOCAL_REPO
-    }
-    if (env.DAOS_STACK_UBUNTU_20_04_LOCAL_REPO) {
-      ret_str += ' --build-arg REPO_UBUNTU_20_04=' + env.DAOS_STACK_UBUNTU_20_04_LOCAL_REPO
-    }
-    if (env.DAOS_STACK_LEAP_15_GROUP_REPO) {
-      ret_str += ' --build-arg REPO_GROUP_LEAP15=' +
-                 env.DAOS_STACK_LEAP_15_GROUP_REPO
+    if (add_repos) {
+      if (env.REPOSITORY_URL) {
+        ret_str += ' --build-arg REPO_URL=' + env.REPOSITORY_URL
+      }
+      if (env.DAOS_STACK_EL_7_LOCAL_REPO) {
+        ret_str += ' --build-arg REPO_EL7=' + env.DAOS_STACK_EL_7_LOCAL_REPO
+      }
+      if (env.DAOS_STACK_EL_8_LOCAL_REPO) {
+        ret_str += ' --build-arg REPO_EL8=' + env.DAOS_STACK_EL_8_LOCAL_REPO
+      }
+      if (env.DAOS_STACK_LEAP_15_LOCAL_REPO) {
+        ret_str += ' --build-arg REPO_LOCAL_LEAP15=' +
+                   env.DAOS_STACK_LEAP_15_LOCAL_REPO
+      }
+      if (env.DAOS_STACK_UBUNTU_20_04_LOCAL_REPO) {
+        ret_str += ' --build-arg REPO_UBUNTU_20_04=' + env.DAOS_STACK_UBUNTU_20_04_LOCAL_REPO
+      }
+      if (env.DAOS_STACK_LEAP_15_GROUP_REPO) {
+        ret_str += ' --build-arg REPO_GROUP_LEAP15=' +
+                   env.DAOS_STACK_LEAP_15_GROUP_REPO
+      }
     }
     if (env.HTTP_PROXY) {
       ret_str += ' --build-arg HTTP_PROXY="' + env.HTTP_PROXY + '"'
