35229ea9d11065ad31717dc6c427a440eba24f85
Marcela Rosales <marcela.a.rosales.jimenez@intel.com>
2020-08-15 00:49:01 +0000

    set WITH_VALGRIND correclty?
    Signed-off-by: Marcela Rosales <marcela.a.rosales.jimenez@intel.com>

--- vars/runTest.groovy
+++ vars/runTest.groovy
@@ -101,22 +101,12 @@ def call(Map config = [:]) {
 
     def cb_result = currentBuild.result
     
-    echo config['with_valgrind']
     environment {
         WITH_VALGRIND = config['with_valgrind']
     }
-    echo env.WITH_VALGRIND
-    if(config['with_valgrind']) {
-        env.WITH_VALGRIND = config['with_valgrind']
-    }
-    echo env.WITH_VALGRIND
 
-    withEnv(['WITH_VALGRIND=${config[with_valgrind]}']) {
-      echo env.WITH_VALGRIND
-      int rc = 0
-      rc = sh(script: script, label: flow_name, returnStatus: true)
-    }
-    echo env.WITH_VALGRIND
+    int rc = 0
+    rc = sh(script: script, label: flow_name, returnStatus: true)
 
     if (cb_result != currentBuild.result) {
       println "The runTest script changed build result to " +
