a07f22a8cc2292384f1e18d54afc3def32ac6ed2
Marcela Rosales <marcela.a.rosales.jimenez@intel.com>
2020-08-31 17:18:45 +0000

    wip
    Signed-off-by: Marcela Rosales <marcela.a.rosales.jimenez@intel.com>

--- vars/unitTest.groovy
+++ vars/unitTest.groovy
@@ -117,7 +117,7 @@ def call(Map config = [:]) {
   params['stashes'] = stashes
   params['script'] = "SSH_KEY_ARGS=${env.SSH_KEY_ARGS} " +
                      "NODELIST=${nodelist} " +
-                     "WITH_VALGRIND=${stage_info['with_valgrind']}" +
+                     "WITH_VALGRIND=${stage_info['with_valgrind']} " +
                      test_script
   params['junit_files'] = config.get('junit_files', 'test_results/*.xml')
   params['context'] = config.get('context', 'test/' + env.STAGE_NAME)
