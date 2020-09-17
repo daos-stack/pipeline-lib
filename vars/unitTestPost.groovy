2f4c3ebfbf45a06ba1acf24186b0cf196b603bdf
Marcela Rosales <marcela.a.rosales.jimenez@intel.com>
2020-09-17 17:56:26 +0000

    add fileZipOperation
    Signed-off-by: Marcela Rosales <marcela.a.rosales.jimenez@intel.com>

--- vars/unitTestPost.groovy
+++ vars/unitTestPost.groovy
@@ -56,7 +56,8 @@ def call(Map config = [:]) {
     fileOperations([fileCopyOperation(excludes: '',
                                       flattenFiles: false,
                                       includes: src_files,
-                                      targetLocation: target_dir)])
+                                      targetLocation: target_dir),
+                   fileZipOperation(target_dir)])
   }
 
   def artifact_list = config.get('artifacts', ['run_test.sh/*', 'vm_test/**'])
