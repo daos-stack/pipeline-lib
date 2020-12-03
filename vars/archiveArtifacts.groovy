60fdcfc8e90d5fbfc11efbf5e4ba724e4b2cf6d5
Brian J. Murrell <brian.murrell@intel.com>
2020-12-03 16:21:06 -0500

    Revert some of "Don't truncate std{out,err} in JUnit results (#172)" (#173)
    This reverts portions of commit 5162a9ca03b2020a4d3371c5d37d8ccb294a0f05
    that converted defs to types and primitives as that was causing:
    
    org.codehaus.groovy.runtime.typehandling.GroovyCastException: Cannot cast object '[]' with class 'java.util.ArrayList' to class 'java.util.Map' due to: groovy.lang.GroovyRuntimeException: Could not find matching constructor for: java.util.Map()
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- vars/archiveArtifacts.groovy
+++ vars/archiveArtifacts.groovy
@@ -7,17 +7,17 @@
  *
  */
 
-void call(Map config = [:]) {
+def call(Map config = [:]) {
     // We really shouldn't even get here if $NO_CI_TESTING is true as the
     // when{} block for the stage should skip it entirely.  But we'll leave
     // this for historical purposes
-    String script = 'if [ "' + env.NO_CI_TESTING + '''" == 'true' ]; then
-                          exit 1
-                      fi
-                      if git show -s --format=%B | grep "^Skip-test: true"; then
-                          exit 1
-                      fi
-                      exit 0\n'''
+    def script = 'if [ "' + env.NO_CI_TESTING + '''" == 'true' ]; then
+                      exit 1
+                  fi
+                  if git show -s --format=%B | grep "^Skip-test: true"; then
+                      exit 1
+                  fi
+                  exit 0\n'''
     int rc = 0
     rc = sh(script: script, label: env.STAGE_NAME + '_archiveArtifacts',
             returnStatus: true)
