5162a9ca03b2020a4d3371c5d37d8ccb294a0f05
Brian J. Murrell <brian.murrell@intel.com>
2020-12-03 11:19:40 -0500

    Don't truncate std{out,err} in JUnit results (#172)
    This will prevent the dreaded "...[truncated 135494 chars]..." in the
    middle of hte Standard Output section of displayed JUnit result.  It
    should then also result in full wrapping instead of the portion after
    the truncation displaying the \n literally.
    
    Also fix up the lack of $NO_CI_TESTING interpolation due to it being
    inside single quotes.
    
    Convert the use of def to specific primatives/types.
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- vars/junit.groovy
+++ vars/junit.groovy
@@ -9,25 +9,34 @@
 
 
 
-def call(String testResults) {
+void call(String testResults) {
     Map config = [:]
     config['testResults'] = testResults
     call(config)
 }
 
-def call(Map config = [:]) {
-    def script = '''if [ "${NO_CI_TESTING}" == 'true' ]; then
-                        exit 1
-                    fi
-                    if git show -s --format=%B | grep "^Skip-test: true"; then
-                        exit 1
-                    fi
-                    exit 0\n'''
+void call(Map config = [:]) {
+    // We really shouldn't even get here if $NO_CI_TESTING is true as the
+    // when{} block for the stage should skip it entirely.  But we'll leave
+    // this for historical purposes
+    String script = 'if [ "' + env.NO_CI_TESTING + '''" == 'true' ]; then
+                          exit 1
+                      fi
+                      if git show -s --format=%B | grep "^Skip-test: true"; then
+                          exit 1
+                      fi
+                      exit 0'''
     int rc = 0
     rc = sh(script: script, label: env.STAGE_NAME + '_junit',
             returnStatus: true)
     if (rc != 0) {
         config['allowEmptyResults'] = true
     }
+    // don't trucate stdio/stdout in JUnit results
+    // this actually shouldn't be necessary as stdio/stderr is supposed to
+    // be preserved for failed results, however:
+    // https://github.com/jenkinsci/junit-plugin/issues/219
+    // https://issues.jenkins.io/browse/JENKINS-27931
+    config['keepLongStdio'] = true
     steps.junit(config)
 }
