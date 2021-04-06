fbdaf4bc4ae0efac80d44323f3f103ef7d3116f2
Brian J. Murrell <brian.murrell@intel.com>
2021-04-06 09:54:06 -0400

    Environment variables have _, not -
    Skip-PR-comments: true
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- vars/daosPackagesVersion.groovy
+++ vars/daosPackagesVersion.groovy
@@ -17,7 +17,7 @@ import groovy.transform.Field
 String daos_latest_version(String next_version, String repo_type='stable') {
     String v = sh(label: "Get RPM packages version",
                   script: 'repoquery --repofrompath=daos,' + env.REPOSITORY_URL +
-                          env["DAOS-STACK-EL-7-" + repo_type.toUpperCase() + "-REPO"] +
+                          env["DAOS_STACK_EL_7_" + repo_type.toUpperCase() + "_REPO"] +
                         '''/ --repoid daos --qf %{version}-%{release} --whatprovides 'daos(x86-64) < ''' +
                                      next_version + '''' |
                                  rpmdev-sort | tail -1''',
