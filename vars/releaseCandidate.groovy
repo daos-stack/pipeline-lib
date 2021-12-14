1de0bb03fadd77b0d69b4fc4a10b00223caf379d
Brian J. Murrell <brian.murrell@intel.com>
2021-12-14 17:13:51 -0500

    Fix release candidate detection (#273)
    To match with - in front of 'rc'.
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- vars/releaseCandidate.groovy
+++ vars/releaseCandidate.groovy
@@ -13,6 +13,6 @@
 boolean call() {
     return !sh(label: "Determine if building (a PR of) an RC",
                script: "git diff-index --name-only HEAD^ | grep -q TAG && " +
-                       "grep -i '[0-9]rc[0-9]' TAG",
+                       "grep -i '[0-9]-rc[0-9]' TAG",
                returnStatus: true)
 }
