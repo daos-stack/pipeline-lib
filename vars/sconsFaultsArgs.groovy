776ddcc5678eb8844246c7e226ce66cc64ab4048
Brian J. Murrell <brian.murrell@intel.com>
2021-12-15 17:13:51 -0500

    Build a dev build if the test tag has "with_fi" in it (#275)
    This is just a hack to be able to enable fault injection until we have a
    build parameter dedicated to this.
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- vars/sconsFaultsArgs.groovy
+++ vars/sconsFaultsArgs.groovy
@@ -12,8 +12,10 @@
 
 boolean call() {
     // The default build will have BUILD_TYPE=dev; fault injection enabled
-    if (cachedCommitPragma('faults-enabled', 'true').toLowerCase() == 'true' &&
-         !releaseCandidate()) {
+    if (params.TestTag.indexOf("with_fi") >= 0 ||
+        (cachedCommitPragma('faults-enabled', 'true').toLowerCase() == 'true' &&
+         !releaseCandidate()))
+         ) {
         return "BUILD_TYPE=dev"
     } else {
         return "BUILD_TYPE=release"
