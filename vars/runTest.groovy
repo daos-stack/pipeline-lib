2d8d5c0cd41396acf9e64e1ac389267f1ec7025e
John E Malmberg <john.e.malmberg@intel.com>
2022-05-13 17:00:44 -0500

    CORCI-1200 Test: Fix quoting
    Signed-off-by: John E Malmberg <john.e.malmberg@intel.com>

--- vars/runTest.groovy
+++ vars/runTest.groovy
@@ -54,7 +54,7 @@ def call(Map config = [:]) {
     def description = config.get('description', env.STAGE_NAME)
     def flow_name = config.get('flow_name', env.STAGE_NAME)
 
-    jobStatus(JUNIT_failure, 'Entered runTest')
+    jobStatus('JUNIT_failure', 'Entered runTest')
 
     dir('install') {
         deleteDir()
