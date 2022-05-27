97e38520cf1178374cf53db20b76fb2b3c363fec
Phillip Henderson <phillip.henderson@intel.com>
2022-05-27 09:38:48 -0400

    Stage 1 re-introduction
    Signed-off-by: Phillip Henderson <phillip.henderson@intel.com>

--- vars/parseStageInfo.groovy
+++ vars/parseStageInfo.groovy
@@ -35,28 +35,44 @@
  *                             Default determined by this function below.
  */
 
+String get_build_params_tags(String param_key) {
+  // Get the tags defined by the build parameter entry for this stage
+  if (startedByUser()) {
+    if (param_key && param_key == 'tcp' && params.TestTagTCP && params.TestTagTCP != '') {
+      return params.TestTagTCP
+    }
+    if (param_key && param_key == 'ucx' && params.TestTagUCX && params.TestTagUCX != '') {
+      return params.TestTagUCX
+    }
+    if (params.TestTag && params.TestTag != '') {
+      return params.TestTag
+    }
+  }
+  return ''
+}
+
 String get_commit_pragma_tags(String pragma_suffix) {
-  // Get the test tags defined in the commit message with the following priority:
-  //  1) Test-tag-<stage>: <tags>
-  //  2) Test-tag: <tags>
-  //  3) Features: <feature_tags>
+  // Get the test tags defined in the commit message
   String pragma_tag
-  pragma_tag = commitPragma("Test-tag" + pragma_suffix, null)
-  if (!pragma_tag) {
-    pragma_tag = commitPragma("Test-tag", null)
-    if (!pragma_tag) {
-      pragma_tag = commitPragma("Features", null)
-      if (pragma_tag) {
-        String features = pragma_tag
-        pragma_tag = 'pr '
-        for (feature in features.split(' ')) {
-          pragma_tag += 'daily_regression,' + feature + ' '
-          /* DAOS-6468 Ideally we'd like to add this but there are too
-                      many failures in the full_regression set 
-          pragma_tag += 'full_regression,' + feature + ' '
-          */
-        }
-      }
+
+  // Use the tags defined by the stage-specific 'Test-tag-<stage>:' commit message pragma.  If those
+  // are not specified use the tags defined by the general 'Test-tag:' commit message pragma.
+  pragma_tag = commitPragma("Test-tag" + pragma_suffix, commitPragma("Test-tag", null))
+  if (pragma_tag) {
+    return pragma_tag
+  }
+
+  // If neither of the 'Test-tag*:' commit message pragmas are specified, use the 'Features:'
+  // commit message pragma to define the tags to use.
+  String features = commitPragma("Features", null)
+  if (features) {
+    // Features extend the standard pr testing tags to include tests run in daily or weekly builds
+    // that test the specified feature.
+    pragma_tag = 'pr'
+    for (feature in features.split(' ')) {
+      pragma_tag += ' daily_regression,' + feature
+      // DAOS-6468 Eventually add this when there are no failures in the full_regression set
+      // pragma_tag += ' full_regression,' + feature
     }
   }
   return pragma_tag
