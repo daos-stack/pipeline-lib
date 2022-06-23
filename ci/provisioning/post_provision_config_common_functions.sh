f830f6838eb1899f3f7523587f9e99574e12fb2d
Phillip Henderson <phillip.henderson@intel.com>
2022-06-23 10:42:03 -0400

    Applying feedback.
    Signed-off-by: Phillip Henderson <phillip.henderson@intel.com>

--- ci/provisioning/post_provision_config_common_functions.sh
+++ ci/provisioning/post_provision_config_common_functions.sh
@@ -191,14 +191,13 @@ set_local_repo() {
     local version
     version="$(lsb_release -sr)"
     version=${version%%.*}
-    if [ "$repo_server" = "artifactory" ] && [ -z "$(rpm_test_version)" ]; then
-        if [[ ${CHANGE_TARGET:-$BRANCH_NAME} != weekly-testing* ]] ||
-           [[ ${CHANGE_TARGET:-$BRANCH_NAME} != provider-testing* ]]; then
-            # Disable the daos repo so that the Jenkins job repo or a PR-repos*: repo is
-            # used for daos packages
-            dnf -y config-manager --disable \
-                daos-stack-daos-"${DISTRO_GENERIC}"-"$version"-x86_64-stable-local-artifactory
-        fi
+    if [ "$repo_server" = "artifactory" ] && [ -z "$(rpm_test_version)" ] &&
+       { [[ ${CHANGE_TARGET:-$BRANCH_NAME} != weekly-testing* ]] ||
+         [[ ${CHANGE_TARGET:-$BRANCH_NAME} != provider-testing* ]]; }; then
+         # Disable the daos repo so that the Jenkins job repo or a PR-repos*: repo is
+         # used for daos packages
+         dnf -y config-manager \
+            --disable daos-stack-daos-"${DISTRO_GENERIC}"-"$version"-x86_64-stable-local-artifactory
     fi
     dnf repolist
 }
