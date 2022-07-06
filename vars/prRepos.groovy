d066cb9fcf07df463771868ae7b40d3485fccc8c
Brian J. Murrell <brian.murrell@intel.com>
2022-07-05 20:35:10 -0400

    CI_PR_REPOS overrides pragmas
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>
    Skip-PR-comments: true

--- vars/prRepos.groovy
+++ vars/prRepos.groovy
@@ -18,20 +18,21 @@ String call(String distro) {
     String repos = ""
     if (params.CI_PR_REPOS) {
         repos = params.CI_PR_REPOS
-    }
-    // TODO: add parameter support for per-distro CI_PR_REPOS
-    if (distro.startsWith('el7') || distro.startsWith('centos7')) {
-        repos = cachedCommitPragma('PR-repos-el7')
-    } else if (distro.startsWith('el8') || distro.startsWith('centos8') ||
-               distro.startsWith('rocky8') || distro.startsWith('almalinux8') ||
-               distro.startsWith('rhel8')) {
-        repos = cachedCommitPragma('PR-repos-el8')
-    } else if (distro.startsWith('leap15')) {
-        repos = cachedCommitPragma('PR-repos-leap15')
-    } else if (distro.startsWith('ubuntu20')) {
-        repos = cachedCommitPragma('PR-repos-ubuntu20')
     } else {
-       error 'prRepos not implemented for ' + distro
+        // TODO: add parameter support for per-distro CI_PR_REPOS
+        if (distro.startsWith('el7') || distro.startsWith('centos7')) {
+            repos = cachedCommitPragma('PR-repos-el7')
+        } else if (distro.startsWith('el8') || distro.startsWith('centos8') ||
+                   distro.startsWith('rocky8') || distro.startsWith('almalinux8') ||
+                   distro.startsWith('rhel8')) {
+            repos = cachedCommitPragma('PR-repos-el8')
+        } else if (distro.startsWith('leap15')) {
+            repos = cachedCommitPragma('PR-repos-leap15')
+        } else if (distro.startsWith('ubuntu20')) {
+            repos = cachedCommitPragma('PR-repos-ubuntu20')
+        } else {
+           error 'prRepos not implemented for ' + distro
+        }
     }
     return [repos,
             cachedCommitPragma('PR-repos')].join(' ').trim()
