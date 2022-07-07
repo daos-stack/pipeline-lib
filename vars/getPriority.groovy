62706866476350c6dfc2b22fad361a8c3307e124
Ashley Pittman <ashley.m.pittman@intel.com>
2022-07-07 13:14:00 +0100

    DAOS-7821 ci: Allow jobs to set priority.
    Signed-off-by: Ashley Pittman <ashley.m.pittman@intel.com>

--- vars/getPriority.groovy
+++ vars/getPriority.groovy
@@ -14,5 +14,17 @@ def call() {
         string p = ''
     }
     println("Setting build priroity to: " + p)
+
+    if (!fileExists('ci/jira_query.py')) {
+        return p
+    }
+
+     p = sh(label: "Determine Job priority",
+            script: ["GITHUB_BASE_REF=${target_branch}",
+		     "PR_TITLE=" + env.CHANGE_TITLE,
+                     "ci/jira_query.py", "--priority-only"].join(' '),
+            returnStatus: true).trim()
+ 
+    println("Setting build priroity to: " + p)
     return p
 }
