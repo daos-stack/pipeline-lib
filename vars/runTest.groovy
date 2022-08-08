6765a77cb67aa57a4b9767f0fe34c4ec492a94c6
John E Malmberg <john.e.malmberg@intel.com>
2022-08-08 17:01:03 -0500

    SRE-460 Fix: Sytax error.
    Signed-off-by: John E Malmberg <john.e.malmberg@intel.com>

--- vars/runTest.groovy
+++ vars/runTest.groovy
@@ -136,7 +136,7 @@ def call(Map config = [:]) {
                     status = 'FAILURE'
                     println 'Found at least one error in the Junit files.'
                 } else if (sh(label: 'Check junit xml files for failures',
-                              script: grep -E "<error( |Details>|StackTrace>)" ' + junit_xml,
+                              script: 'grep -E "<error( |Details>|StackTrace>)" ' + junit_xml,
                               returnStatus: true) == 0) {
                     status = 'UNSTABLE'
                     println 'Found at least one failure in the junit files.'
