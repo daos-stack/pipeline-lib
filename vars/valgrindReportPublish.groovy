a9be1e34fc9b6e64d44040a33e3c8bc014a4f4fa
Brian J. Murrell <brian.murrell@intel.com>
2022-12-02 14:37:44 -0500

    Linting
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>
    Skip-PR-comments: true

--- vars/valgrindReportPublish.groovy
+++ vars/valgrindReportPublish.groovy
@@ -1,3 +1,4 @@
+/* groovylint-disable DuplicateStringLiteral, VariableName */
 // vars/valgrindReportPublish.groovy
 
   /**
@@ -7,58 +8,56 @@
    *
    * @param config Map of parameters passed
    *
-   *
    * config['ignore_failure']      Ignore test failures.  Default false.
    *
    * config['valgrind_pattern']    Pattern for Valgind files.
    *                               Default: '*.memcheck.xml'
    *
    * config['valgrind_stashes']    list of stashes for valgrind results.
-   *
-   *
    */
 
-def call(Map config = [:]) {
-
-  def stashes = []
-  if (config['valgrind_stashes']) {
-    stashes = config['valgrind_stashes']
+void call(Map config = [:]) {
+    List stashes = []
+    if (config['valgrind_stashes']) {
+        stashes = config['valgrind_stashes']
   } else {
-    // Older code publishes the valgrind in the same stage as ran the
-    // vagrind test.
-    // That does not work if you have multiple valgrind stages running.
-    // Need to have only one Valgrind publish stage
-    println "No valgrind_stashes passed!   Running older code!"
-  }
-
-  fileOperations([fileDeleteOperation(includes: '*.memcheck.xml')])
-
-            println("Success unstashing ${stash}.")
-  int stash_cnt=0
-  stashes.each {
-    try {
-      unstash it
-      stash_cnt++
-    } catch(Exception ex) {
-      println("Ignoring failure to unstash ${it}.  Perhaps the stage was skipped?")
+        // Older code publishes the valgrind in the same stage as ran the
+        // vagrind test.
+        // That does not work if you have multiple valgrind stages running.
+        // Need to have only one Valgrind publish stage
+        println 'No valgrind_stashes passed!   Running older code!'
     }
-  }
 
-  if (stash_cnt < 1) {
-    println("No valgrind XML files found, skipping valgrind publishing")
-    return
-  }
+    fileOperations([fileDeleteOperation(includes: '*.memcheck.xml')])
+
+    int stash_cnt = 0
+    stashes.each { stash ->
+        try {
+            unstash stash
+            println("Success unstashing ${stash}.")
+            stash_cnt++
+    /* groovylint-disable-next-line CatchException */
+    } catch (Exception ex) {
+            println("Ignoring failure to unstash ${stash}.  Perhaps the stage was skipped?")
+        }
+    }
+
+    if (stash_cnt < 1) {
+        println('No valgrind XML files found, skipping valgrind publishing')
+        return
+    }
 
     println("Unstashed ${stash_cnt} stashes")
     sh(label: 'Show valgrind files',
        script: 'ls -l *.memcheck.xml',
        returnStdout: true)
 
-  boolean ignore_failure = config.get('ignore_failure', false)
+    boolean ignore_failure = config.get('ignore_failure', false)
 
-  String valgrind_pattern = config.get('valgrind_pattern', '*.memcheck.xml')
-  def cb_result = currentBuild.result
-  publishValgrind failBuildOnInvalidReports: true,
+    String valgrind_pattern = config.get('valgrind_pattern', '*.memcheck.xml')
+    /* groovylint-disable-next-line NoDef, VariableTypeRequired */
+    def cb_result = currentBuild.result
+    publishValgrind failBuildOnInvalidReports: true,
                   failBuildOnMissingReports: !ignore_failure,
                   failThresholdDefinitelyLost: '0',
                   failThresholdInvalidReadWrite: '0',
@@ -71,7 +70,7 @@ def call(Map config = [:]) {
                   unstableThresholdInvalidReadWrite: '0',
                   unstableThresholdTotal: '0'
 
-  if (cb_result != currentBuild.result) {
-    println "The publishValgrind step changed result to ${currentBuild.result}."
-  }
+    if (cb_result != currentBuild.result) {
+        println "The publishValgrind step changed result to ${currentBuild.result}."
+    }
 }
