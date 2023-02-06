2591d160821c971832eae708338b11a89e8d75f0
Brian J. Murrell <brian.murrell@intel.com>
2023-02-06 12:16:54 -0500

    Debug
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>
    Skip-PR-comments: true

--- vars/skipStage.groovy
+++ vars/skipStage.groovy
@@ -259,6 +259,8 @@ boolean call(Map config = [:]) {
                    prRepos('leap15').split().any { i -> i.contains('daos@') } ||
                    skip_stage_pragma('build-leap15-rpm')
         case 'Build DEB on Ubuntu 20.04':
+            println(prRepos('ubuntu20').split() + '.any { i -> i.contains("daos@") } == ' +
+                    prRepos('ubuntu20').split().any { i -> i.contains('daos@') })
             return paramsValue('CI_RPM_ubuntu20_NOBUILD', false) ||
                    target_branch == 'weekly-testing' ||
                    (docOnlyChange(target_branch) &&
