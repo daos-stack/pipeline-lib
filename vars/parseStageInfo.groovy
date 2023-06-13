f9f42814f311d5ab153ab1877c55223dcee517c2
Phil Henderson <phillip.henderson@intel.com>
2023-06-13 15:02:19 -0400

    DAOS-13529 test: Add support of + tag group separator
    A '+' string will be used to define different groups of tags to run with
    different launch.py command lines in the same the test stage.  To enable
    this the cluster size tags should not be added to this 'tag'.
    
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- vars/parseStageInfo.groovy
+++ vars/parseStageInfo.groovy
@@ -343,7 +343,13 @@ Map call(Map config = [:]) {
     // Assemble the stage test tags - add the cluster size to each tag group
     result['test_tag'] = ''
     for (group in tag.split(' ')) {
-      result['test_tag'] += group + ',' + cluster_size + ' '
+      if (group == '+') {
+        // The '+' is a special string in the tag field for defining multiple groups of tags
+        result['test_tag'] += group + ' '
+      }
+      else {
+        result['test_tag'] += group + ',' + cluster_size + ' '
+      }
     }
     result['test_tag'] = result['test_tag'].trim()
 
