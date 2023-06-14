2322123b2154cfbbc5f32732b50e4d17d155dbbe
Phil Henderson <phillip.henderson@intel.com>
2023-06-14 09:09:05 -0400

    Applying feedback
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- vars/branchTypeRE.groovy
+++ vars/branchTypeRE.groovy
@@ -10,14 +10,14 @@ def call(String branch_type) {
         case 'weekly':
             // Match a weekly test branch, e.g. weekly-testing, weekly-<release>-testing, etc.
             // Also support older weekly-testing-<release> branch names
-            return '^weekly-([.-0-9]+)?testing(-2.2)?$'
+            return '^weekly-([-0-9.]+)?testing(-2.2)?$'
 
         case 'provider':
             // Match a provider testing branch, e.g. provider-<provider>-testing,
             // provider-<provider>-<release>-testing, etc.
             // Also support older provider-testing-<provider> and
             // provider-testing-<provider>-<release> branch names
-            return '^provider-([.-0-9a-z]+)?testing(-(tcp|ucx))?(-2.2)?$'
+            return '^provider-([-0-9a-z.]+)?testing(-(tcp|ucx))?(-2.2)?$'
 
         case 'release':
             // Match a release branch name, e.g. release/2.4
