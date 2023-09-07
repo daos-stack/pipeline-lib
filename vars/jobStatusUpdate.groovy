f9b285af6430e50e4c7f43b4dbaea841b2fdfeab
Phil Henderson <phillip.henderson@intel.com>
2023-09-07 14:46:27 -0400

    Fix method return type.
    Skip-daos-build-and-test: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- vars/jobStatusUpdate.groovy
+++ vars/jobStatusUpdate.groovy
@@ -3,33 +3,68 @@
 /**
  * jobStatusUpdate.groovy
  *
- * Update the job status.
+ * Update the job status with a key and Map of test results.
  *
- * @param job_status Map containing the status of the job
- * @return a Map of 
+ * @param key String the santized stage name key for the job result
+ *        result Map of test results from the stage
+ * @return a Map of test results for this stage
  */
-Void call(String key, Map result) {
+Map call(String key, Map result) {
     echo "[jobStatus] Updating result for ${key}"
     Map job_status = [key: [:]]
-    if (result in Map) {
-        job_status.key << result
-    } else {
-        job_status[key] = ['result': value]
-    }
+    job_status.key << result
     return job_status
 }
 
-Void call(Map result) {
+/**
+ * jobStatusUpdate.groovy
+ *
+ * Update the job status with a key and string of test results.
+ *
+ * @param key String the santized stage name key for the job result
+ *        result String of the test result from the stage
+ * @return a Map of test results for this stage
+ */
+Map call(String key, String result) {
+    echo "[jobStatus] Updating result for ${key}"
+    Map job_status = [key: ['result': value]]
+    return job_status
+}
+
+/**
+ * jobStatusUpdate.groovy
+ *
+ * Update the job status with a Map of test results.
+ *
+ * @param result Map of test results from the stage
+ * @return a Map of test results for this stage
+ */
+Map call(Map result) {
     if result == null) {
         return job_status_update()
     }
     return job_status_update(jobStatusKey(env.STAGE_NAME), result)
 }
 
-Void call(String key) {
+/**
+ * jobStatusUpdate.groovy
+ *
+ * Update the job status with the a key.
+ *
+ * @param key String the santized stage name key for the job result
+ * @return a Map of test results for this stage
+ */
+Map call(String key) {
     return job_status_update(key, currentBuild.currentResult)
 }
 
-Void call() {
+/**
+ * jobStatusUpdate.groovy
+ *
+ * Update the job status.
+ *
+ * @return a Map of test results for this stage
+ */
+Map call() {
     return jobStatusUpdate(jobStatusKey(env.STAGE_NAME), currentBuild.currentResult)
 }
