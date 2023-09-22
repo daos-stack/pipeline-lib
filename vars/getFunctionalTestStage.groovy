f0b8076e0fb59b2dbcff7b85a1f5d1089a3a99b9
Phil Henderson <phillip.henderson@intel.com>
2023-09-22 10:46:05 -0400

    Debug
    Skip-daos-build-and-test: true
    
    Signed-off-by: Phil Henderson <phillip.henderson@intel.com>

--- vars/getFunctionalTestStage.groovy
+++ vars/getFunctionalTestStage.groovy
@@ -11,6 +11,7 @@
  *      label           functional test stage default cluster label
  *      next_version    next daos package version
  *      stage_tags      functional test stage tags always used and combined with all other tags
+ *      timer_tags      functional test stage tags to use when the stage is started by a timer
  *      default_tags    launch.py tags argument to use when no parameter or commit pragma exist
  *      default_nvme    launch.py --nvme argument to use when no parameter or commit pragma exist
  *      provider        launch.py --provider argument to use
@@ -19,46 +20,56 @@
  * @return a scripted stage to run in a pipeline
  */
 Map call(Map kwargs = [:]) {
-    echo '[getFunctionalTestStage] Start'
-
     String name = kwargs.get('name')
     String pragma_suffix = kwargs.get('pragma_suffix')
-    String label = cachedCommitPragma('Test-label-' + pragma_suffix, kwargs.get('label'))
+    String label = kwargs.get('label'))
     String next_version = kwargs.get('next_version', null)
     String stage_tags = kwargs.get('stage_tags')
+    String timer_tags = kwargs.get('timer_tags')
     String default_tags = kwargs.get('default_tags')
     String default_nvme = kwargs.get('default_nvme')
     String provider = kwargs.get('provider', '')
     String distro = kwargs.get('distro')
     Map job_status = kwargs.get('job_status', [:])
 
-    String tags = getFunctionalTags(
-        pragma_suffix: pragma_suffix, stage_tags: stage_tags, default_tags: default_tags)
-
-    Map skip_config = ['tags': tags]
-    if (kwargs['distro']) {
-        skip_config['distro'] = kwargs['distro']
-    } else if (pragma_suffix.startsWith('hw-')) {
-        skip_config['hw_size'] = pragma_suffix.replace('hw-', '')
-    }
-
-    echo "[getFunctionalTestStage] Parameters:"
-    echo "[getFunctionalTestStage]   name:          ${name}"
-    echo "[getFunctionalTestStage]   pragma_suffix: ${pragma_suffix}"
-    echo "[getFunctionalTestStage]   label:         ${label}"
-    echo "[getFunctionalTestStage]   next_version:  ${next_version}"
-    echo "[getFunctionalTestStage]   stage_tags:    ${stage_tags}"
-    echo "[getFunctionalTestStage]   default_tags:  ${default_tags}"
-    echo "[getFunctionalTestStage]   tags:          ${tags}"
-    echo "[getFunctionalTestStage]   default_nvme:  ${default_nvme}"
-    echo "[getFunctionalTestStage]   provider:      ${provider}"
-    echo "[getFunctionalTestStage]   distro:        ${distro}"
-    echo "[getFunctionalTestStage]   job_status:    ${job_status}"
-    echo "[getFunctionalTestStage]   skip_config:   ${skip_config}"
-    echo "[getFunctionalTestStage] Start stage ${name}"
-
     return {
         stage("${name}") {
+            label = cachedCommitPragma('Test-label-' + pragma_suffix, label)
+
+            // Get the tags for thge stage. Use the timer_tags if the build has been started by a timer.
+            // Otherwise use either the build parameter, commit pragma, or default tags. All tags are
+            // comnbined with the stage tags to ensure only tests that 'fit' the cluster will be run.
+            if if (startedByTimer() && timer_tags) {
+                default_tags = timer_tags
+            }
+            String tags = getFunctionalTags(
+                pragma_suffix: pragma_suffix, stage_tags: stage_tags, default_tags: default_tags)
+
+            // Setup the arguments for the skipStage() groovy script to directly call the appropraite skip
+            // stage logic. This no longer requires the stage name to be defined in skipStage().
+            Map skip_config = ['tags': tags]
+            if (kwargs['distro']) {
+                skip_config['distro'] = kwargs['distro']
+            } else if (pragma_suffix.startsWith('hw-')) {
+                skip_config['hw_size'] = pragma_suffix.replace('hw-', '')
+            }
+
+            echo "[getFunctionalTestStage] Parameters:"
+            echo "[getFunctionalTestStage]   name:          ${name}"
+            echo "[getFunctionalTestStage]   pragma_suffix: ${pragma_suffix}"
+            echo "[getFunctionalTestStage]   label:         ${label}"
+            echo "[getFunctionalTestStage]   next_version:  ${next_version}"
+            echo "[getFunctionalTestStage]   stage_tags:    ${stage_tags}"
+            echo "[getFunctionalTestStage]   timer_tags:    ${timer_tags}"
+            echo "[getFunctionalTestStage]   default_tags:  ${default_tags}"
+            echo "[getFunctionalTestStage]   tags:          ${tags}"
+            echo "[getFunctionalTestStage]   default_nvme:  ${default_nvme}"
+            echo "[getFunctionalTestStage]   provider:      ${provider}"
+            echo "[getFunctionalTestStage]   distro:        ${distro}"
+            echo "[getFunctionalTestStage]   job_status:    ${job_status}"
+            echo "[getFunctionalTestStage]   skip_config:   ${skip_config}"
+            echo "[getFunctionalTestStage] Start stage ${name}"
+
             if (skipStage(skip_config)) {
                 echo "[${name}] Stage skipped by skipStage(${skip_config})"
             } else {
