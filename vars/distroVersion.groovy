7cdcc4831fad06de174b18b0e4e2f4d4e3626412
Brian J. Murrell <brian.murrell@intel.com>
2023-11-22 13:44:50 -0500

    Add support for Leap 15.5 and EL 8.8 (#397)
    Sync up ci/provisioning.
    
    Add support fot determining EL 8.6 stage.
    
    Use leap-dnf Docker image.
    
    Update testRrpm().
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- vars/distroVersion.groovy
+++ vars/distroVersion.groovy
@@ -24,14 +24,11 @@ String call(String distro) {
 }
 
 String call(String distro, String branch) {
-    return ['centos7':  ['2.2':     '7'],
-            'el8':      ['master': '8.6',
-                         '2.4':    '8.6',
-                         '2.2':    '8.6'],
+    return ['el8':      ['master': '8.8',
+                         '2.4':    '8.6'],
             'el9':      ['master': '9.2'],
-            'leap15':   ['master': '15.4',
-                         '2.4':    '15.4',
-                         '2.2':    '15.4'],
+            'leap15':   ['master': '15.5',
+                         '2.4':    '15.4'],
             'ubuntu20': ['master': '20.04']][distro][branch]
 }
 
