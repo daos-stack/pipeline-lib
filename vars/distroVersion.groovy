042fc2a528e2fd9bcbd2f44666c5abb2e5d08e57
Brian J. Murrell <brian.murrell@intel.com>
2024-01-31 14:20:26 -0500

    Use an alternate algorthim for finding …
    The base branch.
    
    This is the same as is used in our githooks with the exception of
    factoring the "commits behind" into the distance from the base branch.
    
    Use env.CHANGE_TARGET in preference to the above, if it exists.
    
    Skip-PR-comments: true
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>

--- vars/distroVersion.groovy
+++ vars/distroVersion.groovy
@@ -21,22 +21,28 @@ String call(String distro) {
     } else {
         if (env.BASE_BRANCH_NAME) {
             branch = env.BASE_BRANCH_NAME
+        } else if (env.CHANGE_TARGET) {
+            branch = env.CHANGE_TARGET
         } else {
             // find the base branch
             branch = sh(label: 'Find base branch',
+                         /* groovylint-disable-next-line GStringExpressionWithinString */
                          script: '''set -eux -o pipefail
-                                    max_commits=200
-                                    base_branch_re='^  origin/(master|release/)'
-                                    n=0
-                                    while [ "$n" -lt "$max_commits" ]; do
-                                        if git branch -r --contains HEAD~$n |
-                                            grep -E "$base_branch_re" | sed -e 's/^ *[^\\/]*\\///'; then
-                                            exit 0
+                                    ORIGIN=origin
+                                    mapfile -t all_bases < <(echo "master"; git branch -r |
+                                      sed -ne "/^  $ORIGIN\\/release\\/[0-9]/s/^  $ORIGIN\\///p")
+                                    TARGET="master"
+                                    min_diff=-1
+                                    for base in "${all_bases[@]}"; do
+                                        git rev-parse --verify "$ORIGIN/$base" &> /dev/null || continue
+                                        commits_ahead=$(git log --oneline "$ORIGIN/$base..HEAD" | wc -l)
+                                        if [ "$min_diff" -eq -1 ] || [ "$min_diff" -gt "$commits_ahead" ]; then
+                                            TARGET="$base"
+                                            min_diff=$commits_ahead
                                         fi
-                                        ((n++)) || true
                                     done
-                                    echo "Could not find a base branch within $max_commits commits" >&2
-                                    exit 1''',
+                                    echo "$TARGET"
+                                    exit 0''',
                          returnStdout: true).trim().replaceFirst(/^.*(\d+\.\d+).*$/, '\$1')
         }
     }
@@ -54,5 +60,32 @@ String call(String distro, String branch) {
 }
 
 /* groovylint-disable-next-line CompileStatic */
-assert(call('leap15', 'release/2.2') == '15.4')
-assert(call('el8', 'release/2.2') == '8.6')
+assert(call('leap15', '2.4') == '15.5')
+assert(call('leap15', 'master') == '15.5')
+assert(call('el8', '2.4') == '8.8')
+assert(call('el8', 'master') == '8.8')
+
+/* Uncomment to do further testing
+env = [:]
+String branchTypeRE(String foo) {
+    return 'nomatch'
+}
+
+String distroVersion(String distro, String branch) {
+    call(distro, branch)
+}
+
+String sh(Map args) {
+    def sout = new StringBuilder(), serr = new StringBuilder()
+    def cmd = ['/bin/bash', '-c', args['script']]
+    def proc = cmd.execute()
+    proc.consumeProcessOutput(sout, serr)
+    proc.waitForOrKill(1000)
+    //println "out> $sout\nerr> $serr"
+
+    return sout
+}
+
+assert(call('leap15') == '15.5')
+assert(call('el8') == '8.8')
+*/
