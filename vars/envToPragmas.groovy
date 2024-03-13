ad8678d4682a0c50c3cf27d635bfc7cdcf7742ce
Dalton Bohning <dalton.bohning@intel.com>
2024-03-13 21:13:28 +0000

    try trimming and add more test
    Signed-off-by: Dalton Bohning <dalton.bohning@intel.com>

--- vars/envToPragmas.groovy
+++ vars/envToPragmas.groovy
@@ -7,22 +7,12 @@
  * envToPragmas variable
  */
 
-// Map call() {
-//     Map pragmas = [:]
-//     if (env.pragmas)
-//         pragmas = "${env.pragmas}"[1..-2].split(', ').collectEntries { entry ->
-//             String[] pair = entry.split('=', 2)
-//             [(pair.first().trim()): pair.last().trim()]
-//     }
-//     return pragmas
-// }
-
 Map call() {
     Map pragmas = [:]
     if (env.pragmas)
         pragmas = "${env.pragmas}"[1..-2].split(', ').collectEntries { entry ->
-            String[] pair = entry.split('= ')
-            [(pair.first()): " " + pair.last()]  // retain the space for backwards compatibility
+            String[] pair = entry.split('=', 2)
+            [(pair.first().trim()): pair.last().trim()]
     }
     return pragmas
 }
--- vars/selfUnitTest.groovy
+++ vars/selfUnitTest.groovy
@@ -68,11 +68,19 @@ Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>'''
     // We should probably update the internals to always use "as String" so we have better control.
     // Related, envToPragmas will trim the value, so update the expected accordingly.
     // This should eventually be handled by trimming the value in pragmasToMap.
-    // expected_map = [
-    //     "skip-build": "true", "skip-pr-comments": "true","required-githooks": "true",
-    //     "signed-off-by": "Brian J. Murrell <brian.murrell@intel.com>"]
+    expected_map = [
+        "skip-build": "true", "skip-pr-comments": "true","required-githooks": "true",
+        "signed-off-by": "Brian J. Murrell <brian.murrell@intel.com>"]
     result_map = envToPragmas()
     println("  result_map   = ${result_map}")
     println("  expected_map = ${expected_map}")
     assert(result_map == expected_map)
+
+    // Re-assign and make sure it still works
+    env.pragmas = result_map
+    result_map = envToPragmas()
+    println("  result_map   = ${result_map}")
+    println("  expected_map = ${expected_map}")
+    assert(result_map == expected_map)
+
 }
