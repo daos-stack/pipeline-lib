bf1259ba99b7c7a68479c5e87daf6505c0da1148
Dalton Bohning <dalton.bohning@intel.com>
2024-03-13 21:59:59 +0000

    typo
    Signed-off-by: Dalton Bohning <dalton.bohning@intel.com>

--- vars/selfUnitTest.groovy
+++ vars/selfUnitTest.groovy
@@ -82,7 +82,7 @@ Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>'''
 
 Test-tag: foo bar'''
     expected_map['test-tag'] = ' foo bar'
-    updatePragmas(new_commit_message, True)
+    updatePragmas(new_commit_message, true)
     result_map = envToPragmas()
     println("  result_map   = ${result_map}")
     println("  expected_map = ${expected_map}")
