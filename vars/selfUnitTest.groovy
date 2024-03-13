e77417c5dd384ef4408e1a116a9c35fdaa960812
Dalton Bohning <dalton.bohning@intel.com>
2024-03-13 19:16:52 +0000

    fix unit test
    Signed-off-by: Dalton Bohning <dalton.bohning@intel.com>

--- vars/selfUnitTest.groovy
+++ vars/selfUnitTest.groovy
@@ -18,9 +18,14 @@ Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>'''
     Map expected_map = ["skip-build": "true", "skip-pr-comments": "true", "required-githooks": "true", "signed-off-by": "Brian J. Murrell <brian.murrell@intel.com>"]
 
     println("Test pragmasToMap")
-    assert(pragmasToMap(commit_message) == expected_map)
-    assert(pragmasToMap("") == [:])
-    assert(pragmasToMap("foo") == [:])
+    result = pragmasToMap(commit_message)
+    assert(result == expected_map), result + ' != ' expected_map
+
+    result = pragmasToMap("")
+    assert(result == [:]), result + ' != ' [:]
+
+    result = pragmasToMap("foo")
+    assert(result == [:]), result + ' != ' [:]
 
     // println("Test pragmasToEnv")
     // TODO: there is inconsistent behavior between these casts:
