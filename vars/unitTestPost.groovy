de4f01b7231c4939d0bff7c23700a9db3e56859e
Ryon Jensen <ryon.jensen@intel.com>
2024-04-04 12:28:08 -0600

    SRE-2145 ci: Add requiredResult
    The ignoreFailedBuilds parameter was removed in reaction to the
    warnings-ng plug api change. Adding the requiredResult parameter to the
    discoverGitReferenceBuild call should create the same behavior.
    
    Doc-only: false
    
    Signed-off-by: Ryon Jensen <ryon.jensen@intel.com>

--- vars/unitTestPost.groovy
+++ vars/unitTestPost.groovy
@@ -99,7 +99,8 @@ void call(Map config = [:]) {
         discoverGitReferenceBuild(
           referenceJob: config.get('referenceJobName',
                                    'daos-stack/daos/master'),
-          scm: 'daos-stack/daos')
+          scm: 'daos-stack/daos',
+          requiredResult: 'UNSTABLE')
         recordIssues enabledForFailure: true,
                      failOnError: !results['ignore_failure'],
                      // parameter below removed with the warnings-ng 11.2.2
