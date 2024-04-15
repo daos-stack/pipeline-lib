9961eb369c5d7752702aba16901252c08b194ff7
Dalton Bohning <dalton.bohning@intel.com>
2024-04-15 09:56:03 -0700

    DAOS-15411 ci: librarify some pragma functions (#428)
    Add envToPragmas() to convert env.pragmas to a Map.
    
    Add pragmasToMap() to convert string commit message to a Map.
    
    Adjust existing usages to use new functions.
    
    Add updatePragmas().
    
    Add some more unit testing.
    
    Signed-off-by: Dalton Bohning <dalton.bohning@intel.com>

---
+++ vars/pragmasToMap.groovy
@@ -0,0 +1,33 @@
+/* groovylint-disable ParameterName */
+// vars/pragmasToMap.groovy
+
+/**
+ * pragmasToMap.groovy
+ *
+ * pragmasToMap variable
+ */
+
+Map call(String commit_message) {
+    if (!commit_message) {
+        error("Valid commit message required")
+    }
+
+    Map pragmas = [:]
+    // can't use eachLine() here: https://issues.jenkins.io/browse/JENKINS-46988/
+    commit_message.split('\n').each { line ->
+        String key, value
+        try {
+            (key, value) = line.split(':', 2)
+            if (key.contains(' ')) {
+                // this returns from the .each closure, not the method
+                return
+            }
+            pragmas[key.toLowerCase()] = value.trim()
+        /* groovylint-disable-next-line CatchArrayIndexOutOfBoundsException */
+        } catch (ArrayIndexOutOfBoundsException ignored) {
+            // ignore and move on to the next line
+        }
+    }
+
+    return pragmas
+}
