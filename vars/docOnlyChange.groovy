fe705830ed3fdbba519e1ddbe51a6fa8631f9255
Tomasz Gromadzki <tomasz.gromadzki@intel.com>
2024-09-16 09:38:44 +0200

    Fix: Manula fix Doc only commit if no script is savailable
    Doc-only: true
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@intel.com>

--- vars/docOnlyChange.groovy
+++ vars/docOnlyChange.groovy
@@ -16,8 +16,20 @@ boolean call(String target_branch) {
     }
     if (cachedCommitPragma('Doc-only').toLowerCase() == 'false' ||
         !fileExists('ci/doc_only_change.sh')) {
-        return false
-    }
+        return sh(label: "Determine if doc-only change (manual mode)"
+                  script: "set -uex" + \
+                    "if ! git fetch origin ${target_branch}; then" + \
+                        "echo 'Hrm.  Got an error fetching the target branch'" + \
+                        "exit 0" + \
+                    "fi" + \
+                    "if ! merge_base=$(git merge-base FETCH_HEAD HEAD); then" + \
+                        "echo 'Hrm.  Got an error finding the merge base'" + \
+                        "exit 0" + \
+                    "fi" + \
+                    "git diff --no-commit-id --name-only $merge_base HEAD | " +\
+                       "grep -v -e '^docs/' -e '\.md$' -e '(?i)^.*LICENSE.*$'"
+                  returnStatus: true) == 1
+                }
 
     return sh(label: "Determine if doc-only change",
               script: ["CHANGE_ID=${env.CHANGE_ID}",
