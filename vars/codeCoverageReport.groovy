9ea6f8807aa319d43f7bca14d0e5965adfbacfc3
Phil Henderson <phillip.henderson@hpe.com>
2025-04-09 16:40:35 -0400

    Code coverage updates
    Signed-off-by: Phil Henderson <phillip.henderson@hpe.com>

--- vars/codeCoverageReport.groovy
+++ vars/codeCoverageReport.groovy
@@ -11,6 +11,7 @@
  *      label       label to use when running the script
  */
  Integer call(Map kwargs = [:]) {
+    Date startDate = new Date()
     List stashes = kwargs.get('stashes', [])
     String script = kwargs.get('script', 'ci/code_coverage_report.sh')
     String label = kwargs.get('label', 'Code Coverage Report')
@@ -22,8 +23,8 @@
             unstash stash
             println("Successfully unstashed ${stash}.")
             stash_count++
-    /* groovylint-disable-next-line CatchException */
-    } catch (Exception ex) {
+        /* groovylint-disable-next-line CatchException */
+        } catch (Exception ex) {
             println("Ignoring failure to unstash ${stash}.  Perhaps the stage was skipped?")
         }
     }
@@ -31,20 +32,41 @@
     // Nothing to do if no stash exist
     if (stash_count < 1) {
         println('No code coverage stashes found, skipping merged code coverage report')
-        return 0
-    }
-
-    // Run the script
-    try {
-        sh(script: script, label: label)
         return_code = 0
-    } catch (hudson.AbortException e) {
-        // groovylint-disable UnnecessaryGetter
-        // groovylint-disable-next-line NoDef, VariableTypeRequired
-        def rc_val = (e.getMessage() =~ /\d+$/)
-        if (rc_val) {
-            return_code = rc_val[0] as Integer
+    } else {
+        // Run the script
+        try {
+            sh(script: script, label: label)
+            return_code = 0
+        } catch (hudson.AbortException e) {
+            // groovylint-disable UnnecessaryGetter
+            // groovylint-disable-next-line NoDef, VariableTypeRequired
+            def rc_val = (e.getMessage() =~ /\d+$/)
+            if (rc_val) {
+                return_code = rc_val[0] as Integer
+            }
         }
     }
-    return return_code
+
+    // Generate a result for the stage
+    Date endDate = new Date()
+    Integer runTime = durationSeconds(startDate, endDate)
+    String status = 'SUCCESS'
+    if (return_code != 0) {
+        status = 'FAILURE'
+    }
+    Map results = ['result_code': return_code,
+                   'result': status,
+                   'start_date': startDate,
+                   'end_date': endDate,
+                   'runtest_time': runTime]
+
+    String results_map = 'results_map_' + sanitizedStageName()
+    writeYaml file: results_map,
+              data: results,
+              overwrite: true
+    stash name: results_map,
+          includes: results_map
+
+    return results
  }
