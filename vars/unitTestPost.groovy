8f22b9db16fabdeeebf4bb247741277548ef54ff
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2025-05-20 22:00:02 +0200

    SRE-3029 ci: NLT tests failure in recordIssues
    recordIssues expect scm value otherwise it uses random
    repository (as multiply SCM are used in job)
    
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- vars/unitTestPost.groovy
+++ vars/unitTestPost.groovy
@@ -117,7 +117,9 @@ void call(Map config = [:]) {
                       name: 'Node local testing',
                       tool: issues(pattern: 'vm_test/nlt-errors.json',
                                    name: 'NLT results',
-                                   id: 'VM_test')
+                                   id: 'VM_test'),
+                      scm: 'daos-stack/daos'
+
         if (cb_result != currentBuild.result) {
             println(
               "The recordIssues step changed result to ${currentBuild.result}.")
