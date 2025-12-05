9b5db78a0ddc493eefbd22fae6fb2e2b2f1ada3c
Oksana Salyk <oksana.salyk@hpe.com>
2025-12-05 13:08:28 +0100

    change unitPackages.groovy
    Signed-off-by: Oksana Salyk <oksana.salyk@hpe.com>

--- vars/unitPackages.groovy
+++ vars/unitPackages.groovy
@@ -11,7 +11,7 @@
  * Method to return the list of Unit Testing packages
  */
 
-String call() {
+String call(Map args = [:]) {
     String script = 'ci/unit/required_packages.sh'
     if (!fileExists(script)) {
         echo "${script} doesn't exist.  " +
@@ -20,7 +20,7 @@ String call() {
     }
 
     Map stage_info = parseStageInfo()
-    String target = config.get('image_version') ?:
+    String target = args.get('image_version') ?:
         (stage_info['target'] =~ /([a-z]+)(.*)/)[0][1] + stage_info['distro_version']
     boolean quick_build = quickBuild()
 
