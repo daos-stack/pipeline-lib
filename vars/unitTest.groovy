235a5bc7021b34490fd98478b2ea66297509970f
Oksana Sałyk <oksana.salyk@hpe.com>
2026-02-03 14:01:42 +0100

    SRE-3459 ci: improve the target_stash value (#493)
    SRE-3459 ci: improve the target_stash value
    * standardize target var when creating target_stash var
    
    Signed-off-by: Oksana Salyk <oksana.salyk@hpe.com>

--- vars/unitTest.groovy
+++ vars/unitTest.groovy
@@ -148,7 +148,9 @@ Map call(Map config = [:]) {
                  inst_repos: config.get('inst_repos', ''),
                  inst_rpms: inst_rpms)
 
-    String target_stash = image_version ?: stage_info['target']
+    /* el9-gcc-tests */
+    String target_stash = (image_version ?: ${stage_info['target']}).split('\\.')[0]
+
     target_stash += '-' + stage_info['compiler']
     if (stage_info['build_type']) {
         target_stash += '-' + stage_info['build_type']
