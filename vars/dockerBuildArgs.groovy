69b9b23263e315a1a0782e179e1ffec381dc3eff
Tomasz Gromadzki <tomasz.gromadzki@hpe.com>
2026-02-06 13:11:51 +0100

    Remove support for HTTPS_PROXY variable
    Signed-off-by: Tomasz Gromadzki <tomasz.gromadzki@hpe.com>

--- vars/dockerBuildArgs.groovy
+++ vars/dockerBuildArgs.groovy
@@ -73,15 +73,12 @@ String call(Map config = [:]) {
     if (env.DAOS_HTTPS_PROXY) {
         println "DAOS_HTTPS_PROXY: $DAOS_HTTPS_PROXY"
         https_proxy = env.DAOS_HTTPS_PROXY
-    } else if (env.HTTPS_PROXY) {
-        println "HTTPS_PROXY: $HTTPS_PROXY"
-        https_proxy = env.HTTPS_PROXY
     }
     if (https_proxy) {
         ret_str += ' --build-arg HTTPS_PROXY' + '="' + https_proxy + '"'
         ret_str += ' --build-arg DAOS_HTTPS_PROXY' + '="' + https_proxy + '"'
     } else {
-        println "WARNING: Missing HTTPS_PROXY variable in Docker build arguments"
+        println "WARNING: Missing DAOS_HTTPS_PROXY variable in Docker build arguments"
     }
 
     if (config['qb']) {
