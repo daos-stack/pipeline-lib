633079a9dadad2da0c7971081fb183460a441fae
Phil Henderson <phillip.henderson@hpe.com>
2026-07-08 23:24:01 -0400

    Cleanup defaults.
    Signed-off-by: Phil Henderson <phillip.henderson@hpe.com>

--- vars/scriptedTestRpmStage.groovy
+++ vars/scriptedTestRpmStage.groovy
@@ -17,15 +17,16 @@ import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
  *      instRepos             testRpm() inst_repos argument; defaults to daosRepos()
  *      daosPkgVersion        testRpm() daos_pkg_version argument; defaults to
  *                              daosPackagesVersion(next_version())
- *      instRpms              testRpm() inst_rpms argument; defaults to 'mercury-libfabric'
+ *      instRpms              testRpm() inst_rpms argument; defaults to ''
  *      ignoreFailure         testRpm() ignore_failure argument; defaults to false
- *      alwaysScript          script to run always after the test stage; defaults to ''.
+ *      alwaysScript          script to run always after the test stage, e.g.
+ *                              'ci/rpm/test_daos_post.sh'; defaults to ''.
  *      archiveArtifactsArgs  Map of arguments to pass to archiveArtifacts() for the stage
  * @return a scripted stage to run in a pipeline
  */ 
 Map call(Map kwargs = [:]) {
     // General parameters
-    String name = kwargs.get('name', 'Unknown Functional Test Stage')
+    String name = kwargs.get('name', 'Unknown Test RPM Stage')
     Boolean runStage = kwargs.get('runStage', true) as Boolean
     String label = kwargs.get('label')
     String testBranch = kwargs.get('testBranch', '')
@@ -35,7 +36,7 @@ Map call(Map kwargs = [:]) {
         inst_repos: kwargs.get('instRepos', daosRepos()),
         daos_pkg_version: kwargs.get(
             'daosPkgVersion', daosPackagesVersion(kwargs.get('next_version', null))),
-        inst_rpms: kwargs.get('instRpms', 'mercury-libfabric'),
+        inst_rpms: kwargs.get('instRpms', ''),
         ignore_failure: kwargs.get('ignoreFailure', false)
     ]
     String alwaysScript = kwargs.get('alwaysScript', '')
@@ -43,14 +44,11 @@ Map call(Map kwargs = [:]) {
 
     return {
         stage("${name}") {
-            println("[${name}] Starting stage: kwargs keys=${kwargs.keySet()}")
-
             if (!runStage) {
                 println("[${name}] Stage skipped by runStage=false")
                 Utils.markStageSkippedForConditional("${name}")
                 return
             }
-
             node(label) {
                 // Ensure access to any branch provisioning scripts exist
                 if (testBranch) {
--- vars/scriptedUnitTestStage.groovy
+++ vars/scriptedUnitTestStage.groovy
@@ -13,17 +13,19 @@ import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
  *      label                 test stage default cluster label
  *      testBranch            if specified, checkout sources from this branch before running tests
  *      jobStatus             Map of status for each stage in the job/build
- *      target                the target distro to use for the test stage; defaults to 'el9'
+ *      distro                the distro to use for the test stage, e.g. 'el9'; defaults to ''
  *      timeoutTime           unitTest() timeout time in minutes; defaults to 60
  *      instRepos             unitTest() inst_repos argument; defaults to daosRepos()
  *      testScript            unitTest() test_script argument; defaults to ''
  *      withValgrind          unitTest() with_valgrind argument; defaults to ''
- *      alwaysScript          unitTest() always_script argument; defaults to 'ci/unit/test_nlt_post.sh'
+ *      alwaysScript          unitTest() always_script argument, e.g.
+ *                              'ci/unit/test_nlt_post.sh'; defaults to ''
  *      testResults           unitTest() test_results argument; defaults to 'nlt-junit.xml'
  *      unstashOpt            unitTest() unstash_opt argument; defaults to true
  *      unstashTests          unitTest() unstash_tests argument; defaults to false
- *      instRpms              unitTest() inst_rpms argument; defaults to unitPackages(target: target) + ' daos-client-tests'
- *      imageVersion          unitTest() image_version argument; defaults to 'el9.7'
+ *      instRpms              unitTest() inst_rpms argument; defaults to
+ *                              unitPackages(target: distro) + ' daos-client-tests'
+ *      imageVersion          unitTest() image_version argument, e.g 'el9.7'; defaults to ''
  *      provEnvVars           unitTest() prov_env_vars argument; defaults to ''
  *      unitTestPostArgs      Map of arguments to pass to unitTestPost() for the stage
  *      archiveArtifactsArgs  Map of arguments to pass to archiveArtifacts() for the stage
@@ -31,12 +33,12 @@ import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
  */ 
 Map call(Map kwargs = [:]) {
     // General parameters
-    String name = kwargs.get('name', 'Unknown Functional Test Stage')
+    String name = kwargs.get('name', 'Unknown Unit Test Stage')
     Boolean runStage = kwargs.get('runStage', true) as Boolean
     String label = kwargs.get('label')
     String testBranch = kwargs.get('testBranch', '')
     Map jobStatus = kwargs.get('jobStatus', null) ?: [:]
-    String distro = kwargs.get('distro', 'el9')
+    String distro = kwargs.get('distro', '')
 
     // Unit Test stage parameters
     Map unitTestArgs = [
@@ -44,12 +46,12 @@ Map call(Map kwargs = [:]) {
         'inst_repos': kwargs.get('instRepos', daosRepos(distro)),
         'test_script': kwargs.get('testScript', ''),
         'with_valgrind': kwargs.get('withValgrind', ''),
-        'always_script': kwargs.get('alwaysScript', 'ci/unit/test_nlt_post.sh'),
+        'always_script': kwargs.get('alwaysScript', ''),
         'test_results': kwargs.get('testResults', 'nlt-junit.xml'),
         'unstash_opt': kwargs.get('unstashOpt', true),
         'unstash_tests': kwargs.get('unstashTests', false),
         'inst_rpms': kwargs.get('instRpms', unitPackages(target: distro) + ' daos-client-tests'),
-        'image_version': kwargs.get('imageVersion', 'el9.7'),
+        'image_version': kwargs.get('imageVersion', ''),
         'prov_env_vars': kwargs.get('provEnvVars', '')
     ]
     Map unitTestPostArgs = kwargs.get('unitTestPostArgs', null) ?: [:]
@@ -57,14 +59,11 @@ Map call(Map kwargs = [:]) {
 
     return {
         stage("${name}") {
-            println("[${name}] Starting stage: kwargs keys=${kwargs.keySet()}")
-
             if (!runStage) {
                 println("[${name}] Stage skipped by runStage=false")
                 Utils.markStageSkippedForConditional("${name}")
                 return
             }
-
             node(label) {
                 // Ensure access to any branch provisioning scripts exist
                 if (testBranch) {
