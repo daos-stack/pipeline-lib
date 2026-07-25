0f0fe7cc19dcb489a706f37bb4b85ff636aa9166
Phil Henderson <phillip.henderson@hpe.com>
2026-07-24 21:38:35 -0400

    Simplify scriptedDockerStage()
    Signed-off-by: Phil Henderson <phillip.henderson@hpe.com>

--- vars/scriptedDockerStage.groovy
+++ vars/scriptedDockerStage.groovy
@@ -15,37 +15,24 @@ import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
  *          dockerTag               the docker image tag to use for the build
  *          dockerBuildArgs         optional docker build arguments
  *          stepMethod              method to call to run the stage
- *          stepMethodArgs          arguments to pass to the stepMethod; defaults to [:]
- *          installScript           optional script to run to install rpms; defaults to ''
- *          buildScript             optional script to run to build dependencies; defaults to ''
- *          configLog               optional config.log name to archive upon exception
- *          valgrindSconsBuildArgs  optional scons build arguments for valgrind build
- *          generateRpmsScript      optional script to run to generate rpms; defaults to ''
- *          buildRpmPostArgs        optional arguments to pass to buildRpmPost(); defaults to [:]
  *          archiveArtifactsArgs    optional arguments to pass to archiveArtifacts()
  * @return a scripted stage to run in a pipeline
  */
 /* groovylint-disable-next-line MethodSize */
 Map call(Map kwargs = [:]) {
-    String name = kwargs.get('name', '')
+    String name = kwargs.get('name', null)
     Boolean runStage = kwargs.get('runStage', true)
     Map jobStatus = kwargs.get('jobStatus', null) ?: [:]
-    String dockerTag = kwargs.get('dockerTag', 'unknown-docker-tag')
+    String dockerTag = kwargs.get('dockerTag', null)
     String dockerBuildArgs = kwargs.get('dockerBuildArgs', '')
     Closure stepMethod = kwargs.get('stepMethod')
-    Map stepMethodArgs = kwargs.get('stepMethodArgs', null) ?: [:]
-
-    // Optional scripts and arguments that drive the stage steps
-    String installScript = kwargs.get('installScript', '')
-    String buildScript = kwargs.get('buildScript', '')
-    String configLog = kwargs.get('configLog', '')
-    Map valgrindSconsBuildArgs = kwargs.get('valgrindSconsBuildArgs', null) ?: [:]
-    String generateRpmsScript = kwargs.get('generateRpmsScript', '')
-    Map buildRpmPostArgs = kwargs.get('buildRpmPostArgs', null) ?: [:]
     Map archiveArtifactsArgs = kwargs.get('archiveArtifactsArgs', null) ?: [:]
 
     if (!name) {
         error("scriptedDockerStage() requires a stage 'name' argument")
+    }
+    if (!dockerTag) {
+        error("scriptedDockerStage() requires a 'dockerTag' argument")
     }
 
     return {
@@ -64,50 +51,13 @@ Map call(Map kwargs = [:]) {
                 def dockerImage = docker.build(dockerTag, dockerBuildArgs)
                 try {
                     dockerImage.inside {
-                        if (installScript) {
-                            println("[${name}] Running installScript")
-                            sh label: 'Install RPMs',
-                                script: "${installScript}"
-                        }
-                        if (buildScript) {
-                            println("[${name}] Running buildScript")
-                            sh label: 'Build deps',
-                                script: "${buildScript}"
-                        }
                         println("[${name}] Running stepMethod: ${stepMethod?.getClass()?.name}")
-                        jobStatusUpdate(jobStatus, name, stepMethod.call(stepMethodArgs))
-                        if (valgrindSconsBuildArgs) {
-                            println("[${name}] Running valgrind build for NLT")
-                            // For non-release builds, create a separate build with the valgrind
-                            // tag for NLT memcheck testing.  This is necessary to avoid problems
-                            // caused by valgrind being confused by the Go runtime. We don't want
-                            // to use the valgrind build for normal testing because it is much
-                            // slower. BUILD_TYPE=dev is set for PR/dev builds in sconsArgs(), and
-                            // TARGET_TYPE=release is used to select pre-built cached prerequisites.
-                            jobStatusUpdate(jobStatus, name, sconsBuild(valgrindSconsBuildArgs))
-                            sh label: 'Stash valgrind install tree for NLT',
-                               script: 'tar -C / -cf opt-daos-valgrind.tar opt/daos'
-                            stash(name: 'opt-daos-valgrind', includes: 'opt-daos-valgrind.tar')
-                        }
-                        if (generateRpmsScript) {
-                            println("[${name}] Running generateRpmsScript")
-                            sh label: 'Generate RPMs',
-                                script: "${generateRpmsScript}"
-                        }
-                        if (buildRpmPostArgs) {
-                            println("[${name}] Running buildRpmPost()")
-                            buildRpmPost(buildRpmPostArgs)
-                        }
+                        jobStatusUpdate(jobStatus, name, stepMethod.call())
                     }
                 /* groovylint-disable-next-line CatchException */
                 } catch (Exception e) {
                     tryError = e
                     println("[${name}] Caught exception in try: ${tryError}")
-                    if (configLog) {
-                        sh label: 'Archive config.log',
-                           script: "if [ -f config.log ]; then mv config.log ${configLog}; fi"
-                        archiveArtifacts artifacts: "${configLog}", allowEmptyArchive: true
-                    }
                     jobStatusUpdate(jobStatus, name, 'FAILURE')
                     throw tryError
                 } finally {
